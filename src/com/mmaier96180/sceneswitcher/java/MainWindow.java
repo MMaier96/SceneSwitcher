1e6981ae4df7b8f9ebba8cd7ece1498002f88e3a
Marcel Maier <marcelmaier.htc@gmail.com>
2017-10-27 22:43:17 +0200

    defined base structure of project

---
+++ src/com/mmaier96180/sceneswitcher/java/MainWindow.java
@@ -0,0 +1,27 @@
+package com.mmaier96180.sceneswitcher.java;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class MainWindow {
+
+
+	private Stage primaryStage;
+	private static MainWindow instance;
+
+	@FXML
+	public void initialize() {
+	}
+
+	public MainWindow(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	public static MainWindow getInstance() {
+		return instance;
+	}
+
+}
