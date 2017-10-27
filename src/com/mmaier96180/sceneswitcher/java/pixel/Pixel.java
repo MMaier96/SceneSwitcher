81d3cf593bf026d2e4a120b4c5a0a9df9cb2a893
Marcel Maier <marcelmaier.htc@gmail.com>
2017-10-28 01:58:09 +0200

    added pixel object

---
+++ src/com/mmaier96180/sceneswitcher/java/pixel/Pixel.java
@@ -0,0 +1,37 @@
+package com.mmaier96180.sceneswitcher.java.pixel;
+
+import java.awt.Color;
+
+public class Pixel {
+	private double x;
+	private double y;
+	private Color color;
+	
+	public Pixel() {
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+}
--- src/com/mmaier96180/sceneswitcher/java/scene/Scene.java
+++ src/com/mmaier96180/sceneswitcher/java/scene/Scene.java
@@ -2,4 +2,5 @@ package com.mmaier96180.sceneswitcher.java.scene;
 
 public class Scene {
 	private String name;
+	private Pixel actualPixel;
 }
