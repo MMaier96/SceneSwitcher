4e10445c1d4d4af0be6e44d1dae97a65bb0870f7
Marcel Maier <marcelmaier.htc@gmail.com>
2017-10-28 01:58:48 +0200

    added missing import

--- src/com/mmaier96180/sceneswitcher/java/scene/Scene.java
+++ src/com/mmaier96180/sceneswitcher/java/scene/Scene.java
@@ -1,5 +1,7 @@
 package com.mmaier96180.sceneswitcher.java.scene;
 
+import com.mmaier96180.sceneswitcher.java.pixel.Pixel;
+
 public class Scene {
 	private String name;
 	private Pixel actualPixel;
